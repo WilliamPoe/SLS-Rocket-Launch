7b4616c84df138e1feb27f49a2d2dfb1a8ca61c2
William Poe <williamtpoe@gmail.com>
2026-04-14 09:21:37 -0500

    first commit

---
+++ creative/main.js
@@ -0,0 +1,253 @@
+//import three js and all the addons that are used in this script 
+import * as THREE from 'three';
+import { TW } from 'tw';
+import GUI from 'gui';
+import { makeRocket, makeLaunchMount } from './rocket.js';
+
+console.log(`Loaded Three.js version ${THREE.REVISION}`);
+
+// for debugging
+globalThis.THREE = THREE;
+globalThis.TW = TW;
+
+// Create an initial empty Scene
+var scene = new THREE.Scene();
+globalThis.scene = scene;
+
+// ================================================================
+// Build your scene here
+// params for the rocket
+const params = {
+    corestageColor: "#f17022",
+    enginesectionColor: "#ffffff",
+    engineColor: "#808080",
+    mlColor: 0x696563,
+    upperstageElevation: 0,
+    lasElevation: 0,
+    srbSep: 0,
+    launch: false,
+    velocity: 0,
+    rollSpeed: 0,
+    pitchSpeed: 0,
+    rollProgram: false,
+    pitchProgram: false,
+    cameraSpeed: 1.5,
+};
+
+const smokeTex = new THREE.TextureLoader().load('smoke.png');
+
+const smokeMat = new THREE.MeshStandardMaterial({
+    map: smokeTex,
+    transparent: true,
+    opacity: .1,
+    depthWrite: false,
+    roughness: 1,
+    metalness: 0,
+    color: 0xffffff
+}); 
+
+const smokeGeom = new THREE.PlaneGeometry(1,1);
+const smokeTrail = [];
+
+
+// loading the rocekt and ml into the scene
+let rocketParts = makeRocket(params.corestageColor, params.enginesectionColor, params.engineColor, null, scene);
+let launchMount = makeLaunchMount(params.mlColor, params.enginesectionColor, scene);
+
+// Making the accessible for the gui
+let sls = rocketParts.sls;
+let las = rocketParts.las;
+let upperStage = rocketParts.upperStage;
+let srbL = rocketParts.srbL;
+let srbR = rocketParts.srbR;
+
+const rocketPivot = new THREE.Group();
+scene.add(rocketPivot);
+
+rocketPivot.add(sls);
+
+rocketPivot.position.y = 0;
+sls.position.set(0, 0, 0);
+
+function createSmoke(){
+    const lSRBsmoke = new THREE.Mesh(smokeGeom, smokeMat.clone());
+    const rSRBsmoke = new THREE.Mesh(smokeGeom, smokeMat.clone());
+
+    const lNozzel = srbL.getObjectByName("left_nozzel");
+    const rNozzel = srbR.getObjectByName("right_nozzel");
+
+    const lNozzelPos = new THREE.Vector3();
+    const rNozzelPos = new THREE.Vector3();
+    lNozzel.getWorldPosition(lNozzelPos);
+    rNozzel.getWorldPosition(rNozzelPos);
+
+    lSRBsmoke.position.copy(lNozzelPos);
+    rSRBsmoke.position.copy(rNozzelPos);
+
+    const scale = 1 + Math.random();
+    lSRBsmoke.scale.set(scale, scale, scale);
+    rSRBsmoke.scale.set(scale, scale, scale);
+
+    lSRBsmoke.rotation.z = Math.random() * Math.PI;
+    rSRBsmoke.rotation.z = Math.random() * Math.PI;
+
+    scene.add(lSRBsmoke);
+    scene.add(rSRBsmoke);
+    smokeTrail.push(lSRBsmoke);
+    smokeTrail.push(rSRBsmoke);
+}
+
+function updateSmoke() {
+    for (let i = smokeTrail.length - 1; i >= 0; i--) {
+        const p = smokeTrail[i];
+
+        // moves smoke down
+        p.position.y -= 0.1;
+
+        // Slight drift
+        p.position.x += (Math.random() - 1) * 0.02;
+        p.position.z += (Math.random() - 1) * 0.02;
+
+        // Expand
+        p.scale.multiplyScalar(1.04);
+
+        // Fade out
+        p.material.opacity *= .97;
+
+        p.lookAt(camera.position);
+
+        // remove dead particles
+        if (p.material.opacity < 0.02) {
+            scene.remove(p);
+            smokeTrail.splice(i, 1);
+        }
+    }
+}
+
+// ground plane
+const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(600, 600),
+    new THREE.MeshPhongMaterial({ color: 0x228B22,  side: THREE.DoubleSide} )
+);
+ground.rotation.x = -Math.PI / 2;
+ground.position.y = -29;
+scene.add(ground);
+
+// ambient lighting
+const amb = new THREE.AmbientLight(0xffffff, 1);
+scene.add(amb)
+
+// directional lighting
+const light = new THREE.DirectionalLight( 0xffffff , 5);
+light.position.set(50,100,60);
+scene.add(light);
+
+const helper = new THREE.DirectionalLightHelper( light, 5 );
+//scene.add( helper );
+
+scene.fog = new THREE.Fog(0x87CEEB, 50,400);
+
+// loading in the texture for the sky
+const loader = new THREE.TextureLoader();
+loader.load('sky.jpg', function(texture) {
+    scene.background = texture;
+});
+
+// ================================================================
+
+// Create a renderer to render the scene
+var renderer = new THREE.WebGLRenderer();
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,  1, 1000);
+globalThis.camera = camera;
+scene.add(camera);
+camera.position.set(0,0, 100);
+
+// TW.mainInit() initializes TW, adds the canvas to the document,
+// enables display of 3D coordinate axes, sets up keyboard controls
+TW.mainInit(renderer, scene);
+
+function render() {
+    renderer.render(scene, camera);
+}
+render();
+
+const gui = new GUI();
+gui.add(params, 'launch');
+gui.add(sls.position, "y").listen();
+gui.add(params, 'upperstageElevation', 0, 10).step(1).onChange(val => {
+    if (upperStage) upperStage.position.y = val, las.position.y = val
+});
+gui.add(params, 'srbSep', 0, 10).step(1).onChange(val => {
+    if(srbR && srbL) {
+        srbR.position.x =  val;
+        srbL.position.x = -(val);
+    }
+});
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (params.launch && sls) {
+        launchMount.armPivot.rotation.y += 0.008;
+
+        // Wont launch rocket until crew arm in stored
+        if(launchMount.armPivot.rotation.y >= Math.PI ){
+            launchMount.armPivot.rotation.y = Math.PI;
+            for (let i = 0; i < 4; i++) {
+                createSmoke();
+            }
+            params.velocity += 0.002;
+            sls.position.y += params.velocity;
+            // move the camera along with the rocket at same velocity increase
+            camera.position.y += params.velocity/params.cameraSpeed;
+
+            // Wont rotate until the rocket is high enough
+            // Change the 20 to a higher value!!
+            if(sls.position.y >= 20 && rocketPivot.rotation.y <= Math.PI/2){
+                params.rollSpeed += 0.0001;
+                rocketPivot.rotation.y += params.rollSpeed;
+                params.cameraSpeed = 1;
+
+            }
+            // Wont pitch down until it rocket has rotated
+            if (rocketPivot.rotation.y >= Math.PI/2){
+                params.pitchSpeed += 0.004;
+                sls.position.z += params.pitchSpeed;
+                camera.position.x += params.pitchSpeed;
+                
+                const rotateSpeed = 0.002;
+                sls.rotation.x = Math.min(sls.rotation.x + rotateSpeed, 0.5);
+                // allows rockt to speed up significantly once picthing is complete
+                if(sls.rotation.x >= 0.5){
+                    // sls.position.z += .09;
+                    sls.rotation.x = 0.5;
+                } 
+                // Allows velocity to increase significantly once high enough
+                if(sls.position.y > 100){
+                    //sls.position.z += .3;
+                }
+            }
+        }
+    } else {
+        sls.position.y = -1.5;
+        sls.position.z = 0;
+        params.velocity = 0;
+        params.rollSpeed = 0;
+        params.pitchSpeed = 0;
+        params.cameraSpeed = 1.5;
+        rocketPivot.rotation.y = 0;
+        launchMount.armPivot.rotation.y = -Math.PI / 25;
+        sls.rotation.x = 0;
+        camera.position.set(0,0, 100);
+    }
+
+    updateSmoke();
+
+    renderer.render(scene, camera);
+}
+
+animate();
---
+++ creative/rocket.js
@@ -0,0 +1,314 @@
+import * as THREE from 'three';
+
+// Everything should be 1:6 ratio !!
+
+function makeRocket (coreColor, engSecColor, engColor, oldRocket = NULL, scene){
+
+    if (oldRocket) scene.remove(oldRocket);
+
+    const corestageColor =  new THREE.MeshPhongMaterial({color: coreColor, specular: "#808080"});
+    const enginesectionColor = new THREE.MeshPhongMaterial({color: engSecColor, specular: "#808080"});
+    const engineColor =  new THREE.MeshPhongMaterial({color: engColor, specular: "#808080"});
+
+// Whole Rocket
+
+    const sls = new THREE.Group();
+
+// Parts of Rocket
+
+    const las = new THREE.Group();
+    const upperStage = new THREE.Group();
+    const icps = new THREE.Group();
+    const coreStage = new THREE.Group();
+    const srbL = new THREE.Group();
+    const srbR = new THREE.Group();
+    const srb = new THREE.Group();
+
+
+    upperStage.position.y = 0;
+    las.position.y = 0
+    srbR.position.x = 0
+    srbL.position.x = 0
+
+    // Launch abort system
+
+    const lastipGeom = new THREE.ConeGeometry(0.25, 0.5);
+    const lastipMesh = new THREE.Mesh(lastipGeom, enginesectionColor);
+    const lasrocketGeom = new THREE.CylinderGeometry(0.25, 0.25, 3.5);
+    const lasrocketMesh = new THREE.Mesh(lasrocketGeom, enginesectionColor);
+    const lasadapterGeom = new THREE.CylinderGeometry(0.25, 0.7, 2.3);
+    const lasadapterMesh = new THREE.Mesh(lasadapterGeom, enginesectionColor);
+
+    lastipMesh.position.set(0,37.5,0);
+    lasrocketMesh.position.set(0,35.5,0);
+    lasadapterMesh.position.set(0,32.6,0);
+
+    las.add(lastipMesh);
+    las.add(lasrocketMesh);
+    las.add(lasadapterMesh);
+
+    upperStage.add(las);
+
+    // Crew Module
+
+    const crewGeom = new THREE.CylinderGeometry(0.7, 1.9, 2);
+    const crewMesh = new THREE.Mesh(crewGeom, enginesectionColor);
+
+    crewMesh.position.set(0,30.5,0);
+
+    upperStage.add(crewMesh);
+
+    // Encapsulated service module panels
+
+    const esmpGeom = new THREE.CylinderGeometry(1.9, 1.9, 3);
+    const esmpMesh = new THREE.Mesh(esmpGeom, enginesectionColor);
+
+    esmpMesh.position.set(0,28,0);
+
+    upperStage.add(esmpMesh);
+
+    // Orion stage adapter
+
+    const osaGeom = new THREE.CylinderGeometry(1.9, 1.65, 1);
+    const osaMesh = new THREE.Mesh(osaGeom, enginesectionColor);
+
+    osaMesh.position.set(0,26,0);
+
+    upperStage.add(osaMesh);
+
+    // Interim cryogenic propulsion stage
+
+    const lh2TankGeom = new THREE.CylinderGeometry(1.65, 1.65, 2.343);
+    const lh2TankMesh = new THREE.Mesh(lh2TankGeom, enginesectionColor);
+
+    lh2TankMesh.position.set(0,24.4,0);
+
+    icps.add(lh2TankMesh);
+
+    const icpsStrucGeom = new THREE.CylinderGeometry(1.65, 1.32, 1.406);
+    const icpsStrucMesh = new THREE.Mesh(icpsStrucGeom, new THREE.MeshPhongMaterial({color: 0x4d4d4d, specular: "#808080"}));
+
+    icpsStrucMesh.position.set(0,22.6,0);
+    icps.add(icpsStrucMesh);
+
+    const loxTankGeom = new THREE.CapsuleGeometry(1.32, .5);
+    const loxTankMesh = new THREE.Mesh(loxTankGeom, new THREE.MeshPhongMaterial({color: 0xa1a1a1, specular: "#808080"}));
+
+    loxTankMesh.position.set(0,21.7,0); 
+    icps.add(loxTankMesh);
+
+    const icpsEngineGeom = new THREE.CylinderGeometry(0.5, 0.8, 2.8);
+    const icpsEngineMesh = new THREE.Mesh(icpsEngineGeom, engineColor);
+
+    icpsEngineMesh.position.set(0,19.5,0);
+    icps.add(icpsEngineMesh);
+    icps.name = "icps";
+
+    upperStage.add(icps);
+
+    // Launch vehicle stage adapter
+
+    const lvsaGeom = new THREE.CylinderGeometry(1.65, 2.76, 5.5);
+    const lvsaMesh = new THREE.Mesh(lvsaGeom, corestageColor);
+    lvsaMesh.position.set(0,20.5,0);
+    coreStage.add(lvsaMesh);
+
+    // Core stage
+
+    const corestageGeom = new THREE.CylinderGeometry(2.76, 2.76, 35.6);
+    const corestageMesh = new THREE.Mesh(corestageGeom, corestageColor);
+    coreStage.add(corestageMesh);
+
+    const enginesectionGeom = new THREE.CylinderGeometry(2.76, 2.5, 4);
+    const enginesectionMesh = new THREE.Mesh(enginesectionGeom, enginesectionColor);
+    enginesectionMesh.position.set(0,-19.8,0)
+    coreStage.add(enginesectionMesh);
+
+    // Rocket engines
+
+    const rs25Engines = new THREE.Group();
+
+    const engineGeom = new THREE.CylinderGeometry(0.5, 0.8, 2.8);
+    const engineMesh0 = new THREE.Mesh(engineGeom, engineColor);
+    const engineMesh1 = new THREE.Mesh(engineGeom, engineColor);
+    const engineMesh2 = new THREE.Mesh(engineGeom, engineColor);
+    const engineMesh3 = new THREE.Mesh(engineGeom, engineColor);
+
+    engineMesh0.position.set(1.6,-23,0);
+    engineMesh1.position.set(0,-23,1.6);
+    engineMesh2.position.set(-1.6,-23,0);
+    engineMesh3.position.set(0,-23,-1.6);
+
+
+    rs25Engines.add(engineMesh0);
+    rs25Engines.add(engineMesh1);
+    rs25Engines.add(engineMesh2);
+    rs25Engines.add(engineMesh3);
+
+    rs25Engines.rotation.y = (Math.PI /2) /2;
+    coreStage.add(rs25Engines);
+
+    // Solid rocket boosters
+
+    const srbtipGeom = new THREE.ConeGeometry(1.2, 5.2);
+    const srbtip0Mesh = new THREE.Mesh(srbtipGeom, enginesectionColor);
+    const srbtip1Mesh = new THREE.Mesh(srbtipGeom, enginesectionColor);
+    const srbGeom = new THREE.CylinderGeometry(1.2, 1.2, 27.2);
+    const srb0Mesh = new THREE.Mesh(srbGeom, enginesectionColor);
+    const srb1Mesh = new THREE.Mesh(srbGeom, enginesectionColor);
+    const srbskirtGeom = new THREE.CylinderGeometry(1.2, 2, 2.8);
+    const srbskirt0Mesh = new THREE.Mesh(srbskirtGeom, enginesectionColor);
+    const srbskirt1Mesh = new THREE.Mesh(srbskirtGeom, enginesectionColor);
+    const srbnozzleGeom = new THREE.CylinderGeometry(0.5, 1.2, 2.8);
+    const srbnozzle0Mesh = new THREE.Mesh(srbnozzleGeom, engineColor);
+    const srbnozzle1Mesh = new THREE.Mesh(srbnozzleGeom, engineColor);
+
+    srbtip0Mesh.position.set(4,8.2,0);
+    srbtip1Mesh.position.set(-4,8.2,0);
+    srb0Mesh.position.set(4,-8,0);
+    srb1Mesh.position.set(-4,-8,0);
+    srbskirt0Mesh.position.set(4,-21.5,0);
+    srbskirt1Mesh.position.set(-4,-21.5,0);
+    srbnozzle0Mesh.position.set(4,-23.1,0);
+    srbnozzle1Mesh.position.set(-4,-23.1,0);
+
+    srbnozzle1Mesh.name = "left_nozzel";
+    srbnozzle0Mesh.name = "right_nozzel";
+
+    srbR.add(srb0Mesh);
+    srbR.add(srbtip0Mesh);
+    srbR.add(srbskirt0Mesh);
+    srbR.add(srbnozzle0Mesh);
+
+    srbL.add(srb1Mesh);
+    srbL.add(srbtip1Mesh);
+    srbL.add(srbskirt1Mesh);
+    srbL.add(srbnozzle1Mesh);
+
+    srb.add(srbR);
+    srb.add(srbL);
+
+
+    sls.add(las);
+    sls.add(upperStage);
+    sls.add(coreStage);
+    sls.add(srb);
+
+    scene.add(sls);
+
+
+    return {
+        sls,
+        las,
+        upperStage,
+        coreStage,
+        srbL,
+        srbR,
+        srb
+    };
+}
+
+function makeLaunchMount(mlColor, tankColor, scene){
+
+    const ml1 = new THREE.Group();
+    const launchTower = new THREE.Group();
+    const launchMount = new THREE.Group();
+
+    const launchMat = new THREE.MeshPhongMaterial({ color:  mlColor, specular: "#808080" });
+    const tankMat = new THREE.MeshPhongMaterial({ color: tankColor, specular: "#808080" });
+
+    // launch mount
+    const mount = new THREE.Mesh(new THREE.BoxGeometry(22.5,27.5,4.16), launchMat);
+    mount.rotation.set(Math.PI/2,0,0);
+    mount.position.set(0,-27,-4.5);
+
+    // quick disconnect
+    const qdPivot = new THREE.Group();
+
+    qdPivot.position.set(0, -23.5, 0);
+
+    // lox quick disconnect
+    const loxqd  = new THREE.Mesh(new THREE.BoxGeometry(3, 2, 5.5), launchMat);
+    loxqd.position.set(-2, 0, 6.5);
+    loxqd.rotation.set(Math.PI/2,0,.25);
+    qdPivot.add(loxqd);
+
+    // lh2 quick disconnect
+    const lh2qd  = new THREE.Mesh(new THREE.BoxGeometry(3, 2, 5.5), launchMat);
+    lh2qd.position.set(2, 0, 6.5);
+    lh2qd.rotation.set(Math.PI/2,0,-.25);
+    qdPivot.add(lh2qd);
+
+    qdPivot.rotation.x = -Math.PI / 25;
+    
+    const tower  = new THREE.Mesh(new THREE.BoxGeometry(6.66, 6.66, 62.5), launchMat);
+    
+    // launch tower
+    tower.rotation.set(Math.PI/2,0,0);
+    tower.position.set(0, 5, -13.5);
+    
+    const armPivot = new THREE.Group();
+    armPivot.position.set(4, 29, -11);
+    
+    // launch tower crew arm
+    const crewArm = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 15), launchMat);
+    
+    crewArm.position.set(0, 0, 6);
+    
+    armPivot.add(crewArm);
+    armPivot.rotation.y = -Math.PI / 25;
+
+    // lox tank
+    const loxTank = new THREE.Mesh(new THREE.SphereGeometry( 6.5 ), tankMat);
+    loxTank.position.set(-105,-23,-108),
+    scene.add(loxTank);
+
+    // lox line
+    const loxLine = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 130), launchMat);
+    loxLine.rotation.y = Math.PI / 4;
+    loxLine.position.set(-55, -29, -58);
+    scene.add(loxLine);
+
+    // lh2 tank
+    const lh2Tank = new THREE.Mesh(new THREE.SphereGeometry( 6.5 ), tankMat);
+    lh2Tank.position.set(105,-23,-108),
+    scene.add(lh2Tank);
+
+    // lh2 line
+    const lh2Line = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 130), launchMat);
+    lh2Line.rotation.y = -Math.PI / 4;
+    lh2Line.position.set(55, -29, -58);
+    scene.add(lh2Line);
+
+
+    launchMount.add(mount);
+    launchMount.add(qdPivot);
+
+    launchTower.add(tower);
+    launchTower.add(armPivot);
+
+
+    ml1.add(launchMount);
+    ml1.add(launchTower);
+
+    scene.add(ml1);
+    
+
+    return {
+        ml1,
+        mount,
+        qdPivot,
+        lh2qd,
+        loxqd,
+        tower,
+        armPivot,
+        crewArm,
+        loxTank,
+        loxLine,
+        lh2Tank,
+        lh2Line
+    };
+}
+
+export {makeRocket};
+export {makeLaunchMount};
